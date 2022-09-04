c9cfd304092b23aa8cc4de16253e4748bc92786a
hexcoo <92905095+hexcoo@users.noreply.github.com>
2022-09-04 19:54:05 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export $REDIS_HOST
-export $REDIS_PASSWD
-export $FLASK_USER
-export $FLASK_PASSWD
+export REDIS_HOST
+export REDIS_PASSWD
+export FLASK_USER
+export FLASK_PASSWD
 
 alias ll='ls -al'
 service cron start
